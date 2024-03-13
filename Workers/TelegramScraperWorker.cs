225c2495741f0026acbfb1743cd8c927e5dd6e3f
Zoranais <unikronusus@gmail.com>
2024-03-13 23:24:18 +0200

    ffewfewfwefwe

--- Workers/TelegramScraperWorker.cs
+++ Workers/TelegramScraperWorker.cs
@@ -127,7 +127,7 @@ public class TelegramScraperWorker : BackgroundService
                 .Where(x => _config.Value.SOURCE_GROUPS
                     .Any(s => s
                         .Split('/')
-                        .Last() == x.Value.MainUsername))
+                        .Last() == x.Value.MainUsername))f
                 .Select(x => new Group
                 {
                     GroupId = x.Value.ID,
