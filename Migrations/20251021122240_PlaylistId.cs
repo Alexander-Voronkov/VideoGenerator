2a7785b81e83976705ce11e42a63da2dcdaecac1
Oleksii Batyulev <oleksiibatyulev@gmail.com>
2025-10-21 17:44:16 +0300

    wtf rider

---
+++ Migrations/20251021122240_PlaylistId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideoGenerator.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlaylistId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlaylistId",
+                table: "GeneratedVideos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "YoutubePlaylistId",
+                table: "GenerationQueue",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YoutubePlaylistId",
+                table: "GenerationQueue");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlaylistId",
+                table: "GeneratedVideos",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
