d0efc8c9bcf9b091e91fce02f4e4d0688d3cfa73
Oleksandr-Voronkov <pubgplayer29112004@gmail.com>
2025-11-30 14:04:51 +0100

    add migration

--- Infrastructure/DesignTimeDbContextFactory.cs
+++ Infrastructure/DesignTimeDbContextFactory.cs
@@ -8,8 +8,8 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<Applicatio
     public ApplicationDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseNpgsql("User ID=user@wilby.com;Password=secretsecret;Host=158.101.170.96;Port=5432;Database=brainrotdb;Pooling=true;Connection Lifetime=180;");
+        optionsBuilder.UseNpgsql("User ID=user@wilby.com;Password=secretsecret;Host=postgres.xoxol.xyz;Port=5432;Database=brainrotdb;Pooling=true;Connection Lifetime=180;");
 
-        return new ApplicationDbContext(optionsBuilder.Options);
+        return new ApplicationDbContext(optionsBuilder.Options); 
     }
 }
